85d332364cbce7937fed9d6213b96a9eb40b038b
poojapol01 <polpooja05@gmail.com>
2023-11-25 23:22:24 +0530

    Add Data Annotations and Connection String

--- E-Commerce-Website/Models/Category.cs
+++ E-Commerce-Website/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace E_Commerce_Website.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_Website.Models
 {
     public class Category
     {
+        [Key]
         public int Id {  get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
