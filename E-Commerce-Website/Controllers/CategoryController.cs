ac55ef5dec1f3e2bc0c5e3aac47f24109580ed2c
poojapol01 <polpooja05@gmail.com>
2023-12-01 15:51:29 +0530

    Display all Categories

--- E-Commerce-Website/Controllers/CategoryController.cs
+++ E-Commerce-Website/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Commerce_Website.Data;
+using E_Commerce_Website.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce_Website.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
